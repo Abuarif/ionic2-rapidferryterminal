ae701cfa67f94e4af0c6091b2460682b9d3b004c
Abuarif <suhaimi.albakri@gmail.com>
2017-07-05 09:14:10 +0800

    add routetrip class

--- src/models/ferrytrips.ts
+++ src/models/ferrytrips.ts
@@ -1,17 +1,32 @@
 export class Ferrytrips {
-    trips: RouteTimetable = new RouteTimetable();
+    timetable: RouteTimetable = new RouteTimetable();
+    trip: RouteTrip = new RouteTrip();
 }
 
+export class Route {
+    id: number;
+    name: string;
+}
 export class RouteTimetable {
     id: number;
-    
     departure_a: string;
     boarding_a: string;
-
     departure_b: string;
     boarding_b: string;
+    status: boolean;
+}
 
+export class RouteTrip {
+    id: number;
+    route_id: number;
     service_date: string;
-    is_on_time: boolean = true;
-    is_available: boolean = true;
+    route_timetable_id: number;
+    isOnTime: boolean;
+    isAvailable: boolean;
+    location_id: number;
+    color_ontime: string = 'dark';
+    color_full: string = 'dark';
+    time_depart: string;
+    update_by: string;
+    status: boolean;
 }
